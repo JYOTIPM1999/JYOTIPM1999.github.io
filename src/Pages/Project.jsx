81596331ecc6684c867ea227b487a41238a0b500
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-11-28 22:38:31 +0530

    Image changed

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -64,7 +64,7 @@ export const Projects = () => {
                 <div>
                   <h2 className={styles.h2text}>{el.title}</h2>
                   <p className={styles.para}>
-                    <span style={{ color: "#fecd45", fontSize: "bold" }}>
+                    <span style={{ color: "tomato", fontSize: "bold" }}>
                       Description:-{" "}
                     </span>
                     {el.desc}
@@ -88,7 +88,7 @@ export const Projects = () => {
           })}
         </div>
         <div>
-          <img src="./Image/project.webp" className={styles.projectIcon} />
+          <img src="./Image/project.gif" className={styles.projectIcon} />
         </div>
       </div>
     </>
