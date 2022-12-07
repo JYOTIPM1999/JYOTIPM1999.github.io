ca09b012e973a19c271993eba8ef3cafeb827f6e
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-12-07 09:57:02 +0530

    Color changed

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -14,40 +14,64 @@ export const Skills = () => {
           <img src="./Image/skills.gif" />
         </div>
         <div className={styles.skill}>
-          <div data-aos="flip-up">
+          <div
+          // data-aos="flip-up"
+          >
             <img src="./Image/html5.png" alt="html5.png" />
           </div>
-          <div data-aos="flip-up">
+          <div
+          // data-aos="flip-up"
+          >
             <img src="./Image/css3.png" alt="css3.png" />
           </div>
-          <div data-aos="flip-up">
+          <div
+          // data-aos="flip-up"
+          >
             <img src="./Image/javascript.png" alt="javascript.png" />
           </div>
-          <div data-aos="flip-up">
+          <div
+          // data-aos="flip-up"
+          >
             <img src="./Image/react-native.png" alt="react-native.png" />
           </div>
-          <div data-aos="flip-right">
+          <div
+          // data-aos="flip-right"
+          >
             <img src="./Image/redux.png" alt="redux.png" />
           </div>
-          <div data-aos="flip-right">
+          <div
+          // data-aos="flip-right"
+          >
             <img src="./Image/nodejs.png" alt="nodejs.png" />
           </div>
-          <div data-aos="flip-right">
+          <div
+          // data-aos="flip-right"
+          >
             <img src="./Image/expressjs.png" alt="expressjs.png" />
           </div>
-          <div data-aos="flip-right">
+          <div
+          // data-aos="flip-right"
+          >
             <img src="./Image/mongo.png" alt="mongo.png" />
           </div>
-          <div data-aos="fade-right">
+          <div
+          // data-aos="fade-right"
+          >
             <img src="./Image/github.png" alt="github.png" />
           </div>
-          <div data-aos="fade-right">
+          <div
+          // data-aos="fade-right"
+          >
             <img src="./Image/DSA2.png" alt="DSA2.png" />
           </div>
-          <div data-aos="fade-right">
+          <div
+          // data-aos="fade-right"
+          >
             <img src="./Image/npm.png" alt="npm.png" />
           </div>
-          <div data-aos="fade-right">
+          <div
+          // data-aos="fade-right"
+          >
             <img src="./Image/git.png" alt="git.png" />
           </div>
         </div>
