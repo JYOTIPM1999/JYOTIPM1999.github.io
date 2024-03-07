6a74d17a5a65901f6d51bef0bf27b6d1cc1d5442
JYOTIPM1999 <jyotipm1999@gmail.com>
2024-03-07 11:49:24 +0530

    Experience added

--- src/Pages/Experience.jsx
+++ src/Pages/Experience.jsx
@@ -2,6 +2,8 @@ import styles from "./Experience.module.css";
 import { FiExternalLink } from "react-icons/fi";
 import { AiTwotoneBug } from "react-icons/ai";
 import { BsFillJournalBookmarkFill } from "react-icons/bs";
+import { BsPersonWorkspace } from "react-icons/bs";
+import { FaNetworkWired } from "react-icons/fa";
 import "aos/dist/aos.css";
 import AOS from "aos";
 import { useEffect } from "react";
@@ -13,6 +15,35 @@ export const Experience = () => {
     <>
       <h1 className={styles.head}>Experience & Education</h1>
       <div id="EXPERIENCE" className={styles.mainDiv}>
+        <div className={styles.box}>
+          <h2 className={styles.h2text}>
+            Specialist
+            <FaNetworkWired style={{ color: "#05386b" }} />
+          </h2>
+          <h4 className={styles.h4text}>
+            <a
+              href="https://www.pwc.in/"
+              target="_blank"
+              className={styles.college}
+            >
+              Price Waterhouse Coopers <FiExternalLink />
+            </a>
+          </h4>
+
+          {/* <h5 className={styles.h5}>Worked at-:</h5>
+          <p className={styles.ptext}>
+            SPP Associates, Bhubaneswar, (10.2019 - 02.2022)
+          </p> */}
+          <h5 className={styles.h5}>
+            Worked on
+            <ul className={styles.ul}>
+              <li>UI </li>
+              <li>Full Stack development </li>
+              <li>MERN Stack </li>
+              <li>SOC Analyst</li>
+            </ul>
+          </h5>
+        </div>
         <div className={styles.box}>
           <h2 className={styles.h2text}>
             Full Stack Web Development{" "}
@@ -36,36 +67,6 @@ export const Experience = () => {
               <li>JavaScript </li>
               <li>Data Structure and Algorithms </li>
               <li>Soft Skills </li>
-            </ul>
-          </h5>
-        </div>
-
-        <div className={styles.box}>
-          <h2 className={styles.h2text}>
-            Chartered Accountacy
-            <BsFillJournalBookmarkFill style={{ color: "#05386b" }} />
-          </h2>
-          <h4 className={styles.h4text}>
-            <a
-              href="https://www.icai.org/"
-              target="_blank"
-              className={styles.college}
-            >
-              The Institute of Chartered Accountants of India <FiExternalLink />
-            </a>
-          </h4>
-
-          <h5 className={styles.h5}>Worked at-:</h5>
-          <p className={styles.ptext}>
-            SPP Associates, Bhubaneswar, (10.2019 - 02.2022)
-          </p>
-          <h5 className={styles.h5}>
-            Worked on
-            <ul className={styles.ul}>
-              <li>Company Audit </li>
-              <li>Tax Audit</li>
-              <li>Internal Audit </li>
-              <li>Income Tax, GSTR Filling </li>
             </ul>
           </h5>
         </div>
